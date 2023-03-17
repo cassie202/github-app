c2b5d2e4118294edc4e4f84d675a8b8ee519a2b5
Cassandra <umehcassandra@gmail.com>
2023-03-17 10:57:13 +0100

    fix bug

--- src/Nav.js
+++ src/Nav.js
@@ -10,19 +10,7 @@ const Navbar = () => {
       <div>
         <ul className='nav-item'>
           <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/blog">Blog</a>
-          </li>
-          <li>
-            <a href="/projects">Projects</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
+            <a href="/" className="Home-btn">Home</a>
           </li>
         </ul>
       </div>
